eae39dc99bcf68d372b467f444bb90adedb70342
Guruprasad Reddy <guruprasad.reddy@techolution.com>
2024-05-22 14:40:39 +0530

    Added the option to give HOST and PORT through cli on client

--- webclient.py
+++ webclient.py
@@ -1,8 +1,17 @@
 import socket
+import sys
 
-HOST = 'google.com'
-PORT = 80
+# Check if the correct number of arguments is provided
+if len(sys.argv) < 2:
+    print("Usage: python webclient.py <hostname> [port]")
+    sys.exit(1)
 
+# Get the hostname and port from the cli arguments
+HOST = sys.argv[1]
+PORT = int(sys.argv[2]) if len(sys.argv) > 2 else 80
+print("Inputs ", HOST, PORT)
+
+# Create a socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT)) # Connects the socket to the server
     request = f"GET / HTTP/1.1\r\nHost: {HOST}\r\nConnection: close\r\n\r\n"
@@ -15,6 +24,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         response += data
 
 response_str = response.decode() # Decoding bytes to string
+print("Raw data:\n", response)
 print('Response string:\n', response_str)
 
 # headers, _, body = response_str.partition('\r\n\r\n')
--- webserver.py
+++ webserver.py
@@ -4,18 +4,21 @@ HOST = '127.0.0.1'
 PORT = 50007
 
 try:
-    while True:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Prevents from getting Address alredy in use error
-            s.bind((HOST, PORT)) # Binds the socket to host and port
-            s.listen(0) # Listen for incoming connections
-            print(f'Server listening on {HOST}:{PORT}')
+    # Create a socket
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Prevents from getting Address already in use error
+        s.bind((HOST, PORT)) # Binds the socket to host and port
+        s.listen() # Listen for incoming connections
+
+    # Keep the server running forever until an interrupt or error occurs.
+        while True:
+            print(f'Server listening on {HOST}:{PORT}\n')
             conn, addr = s.accept() # Accepts a connection
             with conn:
-                print('Connected by', addr)
+                print(f'Connected by {addr}\n')
                 data_buffer = b'' # Buffer to hold the data received
                 while True:
-                    data = conn.recv(4096) # Received upto 4096 bytes at a time
+                    data = conn.recv(4096) # Receives upto 4096 bytes at a time
                     if not data:
                         break
                     data_buffer += data
@@ -23,8 +26,6 @@ try:
                         break
                 print("Full message recieved: ")
                 print(data_buffer.decode()) # Decode the recieved bytes and print it
-                conn.sendall(data_buffer) # Sned the received bytes back to client as response
+                conn.sendall(data_buffer) # Send the received bytes back to client as response
 except KeyboardInterrupt:
     print("\nServer is shutting down") # When a Keyboard Interrupt occurs, print this message
-finally:
-    s.close() # Close the socket
