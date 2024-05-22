f6c28471b6d34ba5cccd4cdcf2ceacfbe275eaf6
Guruprasad Reddy <guruprasad.reddy@techolution.com>
2024-05-22 14:12:32 +0530

    Added comments

--- webclient.py
+++ webclient.py
@@ -4,17 +4,17 @@ HOST = 'google.com'
 PORT = 80
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
+    s.connect((HOST, PORT)) # Connects the socket to the server
     request = f"GET / HTTP/1.1\r\nHost: {HOST}\r\nConnection: close\r\n\r\n"
-    s.sendall(request.encode())
-    response = b''
+    s.sendall(request.encode()) # Sends the request to the server
+    response = b'' # Buffer to store the received data
     while True:
-        data = s.recv(4096)
-        if not data:
+        data = s.recv(4096) # Receiving data from the server 4096 bytes at a time
+        if not data: # Loop runs till all the data has been recieved
             break
         response += data
 
-response_str = response.decode()
+response_str = response.decode() # Decoding bytes to string
 print('Response string:\n', response_str)
 
 # headers, _, body = response_str.partition('\r\n\r\n')
--- webserver.py
+++ webserver.py
@@ -6,25 +6,25 @@ PORT = 50007
 try:
     while True:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            s.bind((HOST, PORT))
-            s.listen(0)
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Prevents from getting Address alredy in use error
+            s.bind((HOST, PORT)) # Binds the socket to host and port
+            s.listen(0) # Listen for incoming connections
             print(f'Server listening on {HOST}:{PORT}')
-            conn, addr = s.accept()
+            conn, addr = s.accept() # Accepts a connection
             with conn:
                 print('Connected by', addr)
-                data_buffer = b''
+                data_buffer = b'' # Buffer to hold the data received
                 while True:
-                    data = conn.recv(4096)
+                    data = conn.recv(4096) # Received upto 4096 bytes at a time
                     if not data:
                         break
                     data_buffer += data
                     if b"\r\n\r\n" in data_buffer:
                         break
                 print("Full message recieved: ")
-                print(data_buffer.decode())
-                conn.sendall(data_buffer)
+                print(data_buffer.decode()) # Decode the recieved bytes and print it
+                conn.sendall(data_buffer) # Sned the received bytes back to client as response
 except KeyboardInterrupt:
-    print("\nServer is shutting down")
+    print("\nServer is shutting down") # When a Keyboard Interrupt occurs, print this message
 finally:
-    s.close()
+    s.close() # Close the socket
